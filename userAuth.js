fb46c9a8adec5fed58f7cd1ce13c17b948a34aad
jenniw <jenniw@mit.edu>
2023-07-28 12:39:38 -0400

    IDE ate my spacing

--- userAuth.js
+++ userAuth.js
@@ -39,24 +39,23 @@ if (isSlackOauth) {
   ))
 } else if (isTouchstoneSamlAuth) {
   passport.use(new touchstoneSamlStrategy({
-      callbackUrl: callbackURL,
-      entryPoint: process.env.TOUCHSTONE_SAML_ENTRYPOINT_URL,
-      issuer: process.env.TOUCHSTONE_SAML_CERT_ISSUER,
-      cert: process.env.TOUCHSTONE_SAML_CERTIFICATE,
-      privateKey: process.env.TOUCHSTONE_SAML_PRIVATE_KEY,
-      decryptionPvk: process.env.TOUCHSTONE_SAML_DECRYPTION_PRIVATE_KEY,
-      wantAssertionsSigned: true,
-      metadataOrganization: {
-        OrganizationName: {'#text': process.env.TOUCHSTONE_SAML_ORG_NAME},
-        OrganizationDisplayName: {'#text': process.env.TOUCHSTONE_SAML_ORG_DISPLAY_NAME},
-        OrganizationURL: {'#text': process.env.TOUCHSTONE_SAML_ORG_URL}
-      },
-      metadataContactPerson: [{
-        '@contactType': 'support',
-        GivenName: process.env.TOUCHSTONE_SAML_CONTACT_NAME,
-        EmailAddress: process.env.TOUCHSTONE_SAML_CONTACT_EMAIL
-      }]
+    callbackUrl: callbackURL,
+    entryPoint: process.env.TOUCHSTONE_SAML_ENTRYPOINT_URL,
+    issuer: process.env.TOUCHSTONE_SAML_CERT_ISSUER,
+    cert: process.env.TOUCHSTONE_SAML_CERTIFICATE,
+    privateKey: process.env.TOUCHSTONE_SAML_PRIVATE_KEY,
+    decryptionPvk: process.env.TOUCHSTONE_SAML_DECRYPTION_PRIVATE_KEY,
+    wantAssertionsSigned: true,
+    metadataOrganization: {
+      OrganizationName: {'#text': process.env.TOUCHSTONE_SAML_ORG_NAME},
+      OrganizationDisplayName: {'#text': process.env.TOUCHSTONE_SAML_ORG_DISPLAY_NAME},
+      OrganizationURL: {'#text': process.env.TOUCHSTONE_SAML_ORG_URL}
     },
+    metadataContactPerson: [{
+      '@contactType': 'support',
+      GivenName: process.env.TOUCHSTONE_SAML_CONTACT_NAME,
+      EmailAddress: process.env.TOUCHSTONE_SAML_CONTACT_EMAIL
+    }]},
     (profile, done) => done(null, profile)))
 } else {
   // default to google auth
